de26d8f0d0a2bc3a2e9625d2a51fd8fc516b51d3
IdoiaBE <1992ibe@gmail.com>
2025-01-29 09:23:38 +0100

    ListCountries component created and imported

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 
 import '../styles/App.scss';
+import dataJson from '../services/data.json'
 import Header from './Header';
+import ListCountries from './ListCountries';
 
 function App() {
   return (
     <>
     <Header/>
+    <main className='main'>
+      <ListCountries/>
+    </main>
     </>
   );
 }
---
+++ src/components/ListCountries.jsx
@@ -0,0 +1,11 @@
+import '../styles/ListCountries.scss'
+
+function ListCountries() {
+  return (
+    <section className='main_list'>
+        
+    </section>
+  )
+}
+
+export default ListCountries
